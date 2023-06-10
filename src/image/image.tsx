d332411a092f45e81ed315ea2bdbf2b0ddb3bb51
JojoDuke <jojoamankwa@gmail.com>
2023-06-10 21:02:48 +0000

    err

--- src/image/image.tsx
+++ src/image/image.tsx
@@ -24,7 +24,7 @@ const ImagePic: React.FC = () => {
             });
             alert("Account has been created successfully");
         } catch (error) {
-            alert("This is error");
+            alert(error);
         }
     }
 
